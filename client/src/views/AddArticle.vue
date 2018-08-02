74d8a23a60a202e253fa77f2681a46a26e9c1686
Rhesa Utomo <venderblitz@gmail.com>
2018-08-02 17:55:09 +0700

    add article bug

--- client/src/views/AddArticle.vue
+++ client/src/views/AddArticle.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <form>
+    <form @submit.prevent="postArticle">
       <div class="form-group">
         <label>Title</label>
         <input v-model="title" type="text" class="form-control" placeholder="title">
@@ -19,12 +19,33 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data () {
     return {
       title: '',
       content: '',
       category: ''
+    }
+  },
+  methods: {
+    postArticle () {
+      let token = localStorage.getItem('token')
+      axios.post('http://localhost:3000/articles', {
+        title: this.title,
+        content: this.content,
+        category: this.category
+      }, {
+        headers: { token }
+      })
+      .then(response => {
+        console.log(response.data)
+        this.$router.push('/')
+      })
+      .catch(err => {
+        console.log(err.response)
+      })
     }
   }
 }
